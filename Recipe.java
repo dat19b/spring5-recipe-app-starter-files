2821956b01bfae65ec8da0d47e80308299c78804
Claus Bové <clbo@kea.dk>
2019-11-19 10:14:47 +0100

    Create Recipe.java

---
+++ Recipe.java
@@ -0,0 +1,12 @@
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+}
+
